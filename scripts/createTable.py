11b1a850f997f84e2f5a18c2d12845943655524c
phil1436 <philxsb@gmail.com>
2023-12-13 13:10:15 +0100

    make frontend startable without createtable

--- scripts/createTable.py
+++ scripts/createTable.py
@@ -1,7 +1,8 @@
-import requests
 
-url = 'http://localhost:8000/dev/createTables'
-
-response = requests.post(url)
-
-print(response.text)
+try:
+    import requests
+    url = 'http://localhost:8000/dev/createTables'
+    response = requests.post(url)
+    print(response.text)
+except:
+    print("Error while creating tables!")
